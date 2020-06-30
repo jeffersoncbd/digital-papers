77299624b276a97234e9c29b108aeec52d4f2454
Jefferson Carlos <jeff.carlosbd@gmail.com>
2020-06-30 20:46:27 +0000

    Cria página inbox

---
+++ pages/inbox/index.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import Router from 'next/router'
+
+import List from '../../components/List'
+import Input from '../../components/Input'
+
+import { Container } from './styles'
+
+const Inbox: React.FC = () => {
+  const [newItem, setNewItem] = useState('')
+  const [items, setItems] = useState([])
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    if (!(newItem === '' || newItem === undefined)) {
+      setNewItem('')
+    }
+  }
+
+  return (
+    <Container>
+      <h1>Caixa de entrada</h1>
+      <form onSubmit={handleSubmit}>
+        <Input
+          placeholder="Adicionar novo"
+          value={newItem}
+          onChange={(event) => setNewItem(event.target.value)}
+        />
+      </form>
+      <List
+        items={items.map((item) => ({
+          key: item.id,
+          primary: item.title,
+          secondary: item.dueDate,
+          onClick: () => Router.push(`/inbox/${item.id}`)
+        }))}
+      />
+    </Container>
+  )
+}
+
+export default Inbox
---
+++ pages/inbox/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  input {
+    margin: 7px 0 5px;
+    border: 1px solid #ddd;
+    width: 100%;
+    height: 47px;
+    border-radius: 4px;
+    padding: 0 15px;
+  }
+`
