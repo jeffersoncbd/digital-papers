87a2d5eb57244940c1f0f716c0cfc9b30fba83f1
Jefferson Carlos <jeff.carlosbd@gmail.com>
2020-07-01 12:17:11 +0000

    Altera estilos do container

--- components/Container/styles.ts
+++ components/Container/styles.ts
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   width: 100%;
-  max-width: 700px;
-  margin: 0 auto;
+  max-width: 800px;
+  margin: 24px auto;
   h1 {
     font-weight: 300;
   }
