3e7f99ef10037199721b822e9e720ef8c4f22c95
Jefferson Carlos <jeff.carlosbd@gmail.com>
2020-07-14 22:14:37 +0000

    Commit para migrar ambiente

--- services/api.ts
+++ services/api.ts
@@ -4,4 +4,14 @@ const api = axios.create({
   baseURL: `/api/`
 })
 
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.message.includes('code 403')) {
+      store.dispatch(logout())
+    }
+    return Promise.reject(error)
+  }
+)
+
 export default api
